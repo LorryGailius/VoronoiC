317265ccc0fa6cd9815cd20a42898448fec56875
Laurynas Gailius <lauris.gailius@gmail.com>
2022-09-13 16:23:41 +0300

    Added SDL support

--- main.c
+++ main.c
@@ -1,55 +1,76 @@
+#include "SDL.h"
 #include <voronoi.h>
+#include <assert.h>
+#include <time.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+const int screen_width = 800;
+const int screen_height = 600;
 
-int main(int argc, char **argv) {
-
+int main(int argc, char *argv[]) {
     srand(time(NULL));
 
-    pixel_t **pixels = (pixel_t**)malloc(SCREEN_HEIGHT * sizeof(pixel_t*));
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Event event;
 
-    for (size_t i = 0; i < SCREEN_HEIGHT; i++)
-    {
-        pixels[i] = malloc(SCREEN_WIDTH * sizeof(pixel_t));
-    }
-    
-    if (pixels == NULL)
-    {
-        puts("Memory error!");
-        return 0;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
     }
 
-    point_t *points = (point_t*)malloc(MAX_POINTS * sizeof(point_t));
+    if (SDL_CreateWindowAndRenderer(screen_width, screen_height, SDL_WINDOW_SHOWN, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
 
-    int size = 0;
-    
-    /*
-    add_point(points, &size, 1, 10);
-    add_point(points, &size, 50, 50);
-    add_point(points, &size, 80, 60);
-    add_point(points, &size, 100, 40);
-    */
+    int mouse_x, mouse_y, pointNum = 0;
 
-    generate_points(points, &size, SCREEN_WIDTH, SCREEN_HEIGHT, MAX_POINTS);
+    pixel_t **pixels = (pixel_t**)malloc(screen_height * sizeof(pixel_t*));
 
-    voronoi(points, size, SCREEN_WIDTH, SCREEN_HEIGHT, pixels);
+    for (size_t i = 0; i < screen_height; i++) {
 
-    for (size_t i = 0; i < SCREEN_HEIGHT; i++) {
-        
-        for (size_t j = 0; j < SCREEN_WIDTH; j++){
+        pixels[i] = (pixel_t*)malloc(screen_width * sizeof(pixel_t));
+    }
 
+    point_t *points = malloc(MAX_POINTS * sizeof(point_t));
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderPresent(renderer);
+
+    while (1) {
+
+        SDL_PollEvent(&event);
+
+        if (event.type == SDL_QUIT) {
+            break;
         }
-        
+        else if(event.type == SDL_MOUSEBUTTONDOWN) {
+            if(event.button.button == SDL_BUTTON_LEFT) { 
+
+                SDL_GetMouseState(&mouse_x, &mouse_y);
+                add_point(points, &pointNum, mouse_x, mouse_y); 
+                voronoi(points, pointNum, screen_width, screen_height, pixels);
+                
+                for (size_t i = 0; i < screen_height; i++) {
+                    for (size_t j = 0; j < screen_width; j++) {
+                        SDL_SetRenderDrawColor(renderer, pixels[i][j].r, pixels[i][j].g, pixels[i][j].b, 255);
+                        SDL_RenderDrawPoint(renderer, j, i);
+                    }
+                }
+                SDL_RenderPresent(renderer);
+            }
+        }
     }
 
-    for(size_t i = 0; i < SCREEN_HEIGHT; i++)
-    {
-        free(pixels[i]);
-    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    for(size_t i = 0; i < screen_height; i++) { free(pixels[i]); }
     
     free(pixels);
+    free(points);
 
-    return 0; 
-
+    return 0;
 }
